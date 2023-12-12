e0be687825b7551d8ef08e629a7d8264494fe434
Daralams <strongpeople006@gmail.com>
2023-12-12 15:58:17 +0700

    add button for filter merk

---
+++ src/components/DataMerk.jsx
@@ -0,0 +1,129 @@
+  import Vivo from '../assets/vivo.png'
+  import Samsung from '../assets/samsung.jpg'
+  import Oppo from '../assets/oppo.png'
+  import Redmi from '../assets/redmi.jpg'
+  import Realme from '../assets/realme.jpg'
+  
+  const DataMerk = [
+    {
+      id : "1",
+      pic : Vivo,
+      merk : "VIVO",
+      seri : "Y17s",
+      price: "1.599.000"
+    },
+    {
+      id : "2",
+      pic : Vivo,
+      merk : "VIVO",
+      seri : "Y27s",
+      price: "2.399.000"
+    },
+    {
+      id : "3",
+      pic : Vivo,
+      merk : "VIVO",
+      seri : "V29e",
+      price: "4.399.000"
+    },
+    {
+      id : "4",
+      pic : Oppo,
+      merk : "OPPO",
+      seri : "RENO 3",
+      price: "4.699.000"
+    },
+    {
+      id : "5",
+      pic : Oppo,
+      merk : "OPPO",
+      seri : "RENO 5",
+      price: "5.699.000"
+    },
+    {
+      id : "6",
+      pic : Oppo,
+      merk : "OPPO",
+      seri : "RENO 6",
+      price: "6.399.000"
+    },
+    {
+      id : "7",
+      pic : Samsung,
+      merk : "SAMSUNG",
+      seri : "Galaxy s23 Ultra",
+      price: "8.399.000"
+    },
+    {
+      id : "8",
+      pic : Samsung,
+      merk : "SAMSUNG",
+      seri : "samsung ZFlip",
+      price: "8.399.000"
+    },
+    {
+      id : "9",
+      pic : Samsung,
+      merk : "SAMSUNG",
+      seri : "ZFlip 2",
+      price: "7.299.000"
+    },
+    {
+      id : "10",
+      pic : Samsung,
+      merk : "SAMSUNG",
+      seri : "Galaxy a54",
+      price: "5.299.000"
+    },
+    {
+      id : "11",
+      pic : Redmi,
+      merk : "REDMI",
+      seri : "redmi 10",
+      price: "4.099.000"
+    },
+    {
+      id : "12",
+      pic : Redmi,
+      merk : "REDMI",
+      seri : "note 11 pro",
+      price: "4.799.000"
+    },
+    {
+      id : "13",
+      pic : Redmi,
+      merk : "REDMI",
+      seri : "note 12",
+      price: "3.699.000"
+    },
+    {
+      id : "14",
+      pic : Realme,
+      merk : "REALME",
+      seri : "Book Prime",
+      price: "5.699.000"
+    },
+    {
+      id : "15",
+      pic : Realme,
+      merk : "REALME",
+      seri : "11 pro",
+      price: "4.299.000"
+    },
+    {
+      id : "16",
+      pic : Realme,
+      merk : "REALME",
+      seri : "C55",
+      price: "4.999.000"
+    },
+    {
+      id : "17",
+      pic : Realme,
+      merk : "REALME",
+      seri : "x50 pro",
+      price: "4.199.000"
+    },
+    ];
+
+export default DataMerk
--- src/components/product-section.jsx
+++ src/components/product-section.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 import ListPic from '../assets/hp-product.png';
-import ListMerkVivo from '../assets/vivo.png';
+import DataMerk from './DataMerk';
 
 function Product() {
   const [showMoreBestSeller, setShowMoreBestSeller] = useState(0);
-  const [selectedBrand, setSelectedBrand] = useState(0);
+  const [selectedBrand, setSelectedBrand] = useState(null);
+  
+  //filter merk hp
+  const [item, setItem] = useState(DataMerk)
+  const menuItem = [...new Set(DataMerk.map((value) => value.merk))]
+  const filterItem = (merkCategory) => {
+    const newItem = DataMerk.filter((newValue) => newValue.merk === merkCategory)
+    setItem(newItem)
+  }
 
   const bestSellerProducts = [
     { name: 'Vivo', id: 1 },
@@ -15,17 +23,6 @@ function Product() {
     { name: 'Samsung Z flip', id: 6 },
     { name: 'Rog', id: 7 },
     { name: 'Huawei', id: 8 }
-  ];
-
-  const merkListHp = [
-    { merk: 'vivo Y17s', price: '1.599.000', id: 1 },
-    { merk: 'vivo Y27s', price: '2.399.000', id: 2 },
-    { merk: 'vivo V29e', price: '4.399.000', id: 3 },
-    { merk: 'vivo V29', price: '5.499.000', id: 4 },
-    { merk: 'vivo Y27 5G', price: '2.599.000', id: 5 },
-    { merk: 'vivo Y02t', price: '1.399.000', id: 6 },
-    { merk: 'vivo Y36 5G', price: '3.499.000', id: 7 },
-    { merk: 'vivo V25pro', price: '5.699.000', id: 8 }
   ];
 
   const renderProductSection = (sectionProducts, sectionTitle, showMore, setShowMore) => {
@@ -62,28 +59,36 @@ function Product() {
         <div className="pt-6">
           <h2 className="text-2xl text-center mb-4">Find the right phone for you</h2>
           <div className="mt-3 py-3 px-4 flex justify-center rounded-lg shadow-lg">
-            <div className="flex flex-wrap justify-center gap-3">
-              <button className={`py-2 px-4 ${selectedBrand === 1 ? 'bg-blue-400 text-white' : 'bg-transparent border-2 border-blue-400 text-blue-400'} rounded`} onClick={() => setSelectedBrand(1)}>VIVO</button>
-              <button className={`py-2 px-4 ${selectedBrand === 2 ? 'bg-blue-400 text-white' : 'bg-transparent border-2 border-blue-400 text-blue-400'} rounded hover:bg-blue-400 hover:text-white`} onClick={() => setSelectedBrand(2)}>OPPO</button>
-              <button className={`py-2 px-4 ${selectedBrand === 3 ? 'bg-blue-400 text-white' : 'bg-transparent border-2 border-blue-400 text-blue-400'} rounded hover:bg-blue-400 hover:text-white`} onClick={() => setSelectedBrand(3)}>SAMSUNG</button>
-              <button className={`py-2 px-4 ${selectedBrand === 4 ? 'bg-blue-400 text-white' : 'bg-transparent border-2 border-blue-400 text-blue-400'} rounded hover:bg-blue-400 hover:text-white`} onClick={() => setSelectedBrand(4)}>REDMI</button>
-              <button className={`py-2 px-4 ${selectedBrand === 5 ? 'bg-blue-400 text-white' : 'bg-transparent border-2 border-blue-400 text-blue-400'} rounded hover:bg-blue-400 hover:text-white`} onClick={() => setSelectedBrand(5)}>REALME</button>
-              <button className={`py-2 px-4 ${selectedBrand === 6 ? 'bg-blue-400 text-white' : 'bg-transparent border-2 border-blue-400 text-blue-400'} rounded hover:bg-blue-400 hover:text-white`} onClick={() => setSelectedBrand(6)}>INFINIX</button>
+            <div className="flex flex-wrap justify-center gap-3">{menuItem.map(value => (
+            
+              <button onClick={() => {
+                filterItem(value);  
+                setSelectedBrand((prevBrand) => (prevBrand === value ? null : value));
+                }
+              } 
+              className={`py-2 px-4 ${selectedBrand === value ? 'bg-blue-400 text-white' : 'bg-transparent border-2 border-blue-400 text-blue-400'} rounded`}>{value}</button>
+            ))}
+            <button onClick={() => {
+            setItem(DataMerk);
+            setSelectedBrand(null);
+            }
+          } 
+          className={`py-2 px-4  ${selectedBrand === null ? 'bg-blue-400 text-white' : 'bg-transparent border-2 border-blue-400 text-blue-400'} rounded`}>ALL</button>
             </div>
           </div>
   
           <div className="pt-8">
-            <div className="flex flex-wrap justify-center gap-3">
-              {merkListHp.map((merkList) => (
-                <div key={merkList.id} className="p-2 w-[200px] rounded-md border-2 border-slate-100 hover:shadow-lg">
-                  <img src={ListMerkVivo} alt="vivo" />
-                  <p className="pt-2 text-lg text-center">{merkList.merk}</p>
-                  <p className="pt-2 text-md text-center">{merkList.price}</p>
+            <div className="flex flex-wrap justify-center gap-3">{item.map((value) => (
+              <div key={value.id} className="p-2 w-[200px] rounded-md border-2 border-slate-100 hover:shadow-lg">
+                  <img src={value.pic} alt="Handphone" />
+                  <p className="pt-2 text-lg text-center">{value.merk}</p>
+                  <p className="text-md text-center">{value.seri}</p>
+                  <p className="pt-2 text-md text-center">{value.price}</p>
                   <div className="flex justify-center mt-2">
                     <button className="bg-blue-400 text-white py-1 px-3 rounded">Buy</button>
                   </div>
                 </div>
-              ))}
+            ))}
             </div>
           </div>
         </div>
